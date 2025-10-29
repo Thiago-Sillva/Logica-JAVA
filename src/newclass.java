b50ea8409250ae3d194a2310f03d8956fc866727
Thiago <thiago.oliver.silva222@gmail.com>
2025-10-28 23:03:28 -0300

    nova classe

---
+++ src/newclass.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class newclass {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+
+    System.out.println("Digite seu nome : ");
+    String nome = sc.nextLine();
+
+        System.out.println("Foi um prazer ter voce aqui : " + nome);
+
+    }
+}
